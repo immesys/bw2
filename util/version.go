36edacfc6347e11a6aa25479b0401a0fc7711e7f
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-07-11 09:57:40 -0700

    Tweak version

--- util/version.go
+++ util/version.go
@@ -1,7 +1,7 @@
 package util
 
 //The version of BW2 this is
-const BW2Version = "2.4.0- 'Hadron'"
+const BW2Version = "2.4.0 'Hadron'"
 const BW2VersionMajor = 2
 const BW2VersionMinor = 4
 const BW2VersionSubminor = 0

458d0538d4037ac01673a04302e907235d7b2cb5
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-07-12 14:21:46 -0700

    Fix alias contract

--- contracts/alias.sol
+++ contracts/alias.sol
@@ -3,8 +3,11 @@ contract Alias {
   mapping (uint256 => bytes32) public DB;
   mapping (bytes32 => uint256) public AliasFor;
 
-  /* The cost of registering an alias */
-  uint256 public AliasPrice;
+  /* The cost of registering a long alias, multiplied by the gas price */
+  uint256 public LongAliasPrice;
+
+  /* The cost of registering a short alias, multiplied by the gas price */
+  uint256 public ShortAliasPrice;
 
   /* The last short alias assigned */
   uint256 public LastShort;
@@ -19,8 +22,9 @@ contract Alias {
   event AliasCreated(uint256 indexed key, bytes32 indexed value);
 
   function Alias() {
-    AliasPrice = 1 ether;
     LastShort = 0x100;
+    LongAliasPrice = 5000000
+    ShortAliasPrice = 100000
     Admin = msg.sender;
     /* This is 16 bytes. If you want a long alias that is shorter than
        16 bytes, just left align it rather than right align */
@@ -30,7 +34,7 @@ contract Alias {
   function SetAlias(uint256 k, bytes32 v)
   {
     /* Need to pay for alias creation */
-    if (msg.value < AliasPrice) {
+    if (msg.value < LongAliasPrice*tx.gasprice) {
       throw;
     }
     /* Cannot assign in short alias range */
@@ -41,7 +45,7 @@ contract Alias {
     if (DB[k] != 0x0) {
       throw;
     }
-    if (AliasFor[v] != 0x0) {
+    if (AliasFor[v] == 0x0) {
       AliasFor[v] = k;
     }
     DB[k] = v;
@@ -51,19 +55,15 @@ contract Alias {
   function CreateShortAlias(bytes32 v)
   {
     /* Need to pay for alias creation */
-    if (msg.value < AliasPrice) {
+    if (msg.value < ShortAliasPrice*tx.gasprice) {
       throw;
     }
     LastShort += 1;
     DB[LastShort] = v;
-    if (AliasFor[v] != 0x0) {
+    if (AliasFor[v] == 0x0) {
       AliasFor[v] = LastShort;
     }
     AliasCreated(LastShort, v);
   }
 
-  function Resolve(uint256 k) constant returns (bytes32 v)
-  {
-    return DB[k];
-  }
 }

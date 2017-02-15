cb3efcc0479e9375bf06165f95e8fff0c5a74028
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-02-15 08:46:51 -0800

    Version bump

--- util/version.go
+++ util/version.go
@@ -1,8 +1,8 @@
 package util
 
 //The version of BW2 this is
-const BW2Version = "2.5.5 'Iridium'"
+const BW2Version = "2.5.6 'Iridium'"
 const BW2VersionMajor = 2
 const BW2VersionMinor = 5
-const BW2VersionSubminor = 5
+const BW2VersionSubminor = 6
 const BW2VersionName = "Iridium"

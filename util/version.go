15473f560676543047ae0b2605905f491f1f6e1e
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-11-27 15:20:23 -0800

    Bump ver

--- util/version.go
+++ util/version.go
@@ -1,8 +1,8 @@
 package util
 
 //The version of BW2 this is
-const BW2Version = "2.7.6 'Klystron'"
+const BW2Version = "2.8.0 'Lepton'"
 const BW2VersionMajor = 2
-const BW2VersionMinor = 7
-const BW2VersionSubminor = 6
-const BW2VersionName = "Klystron"
+const BW2VersionMinor = 8
+const BW2VersionSubminor = 0
+const BW2VersionName = "Lepton"
